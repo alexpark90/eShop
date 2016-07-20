15bc68b29d07ce9068eaa331f090fc6924bae248
Park, Alex <alexpark90@hotmail.com>
2016-07-20 15:32:57 -0400

    add swiper plugin and set up basic code for the slides

--- js/myscript.js
+++ js/myscript.js
@@ -3,3 +3,16 @@
  *  Date: 
  *	This is the main javascript for eShop
  */
+
+$(document).ready(function () {
+
+	//initialize swiper when document ready  
+	var mySwiper = new Swiper ('.swiper-container', {
+		
+		direction: 'horizontal',
+  		loop: true,
+	    pagination: '.swiper-pagination',
+	    nextButton: '.swiper-button-next',
+	    prevButton: '.swiper-button-prev'
+	});
+});
